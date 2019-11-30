9a2c86890a803cb609e0a72601d52041744d30b2
Ran Loock <ran.trisk@gmail.com>
2019-11-30 15:39:13 +0200

    updates for ecw

--- app/src/main/java/com/bgvofir/grappygis/LayerCalloutDialog/ArrayDump.kt
+++ app/src/main/java/com/bgvofir/grappygis/LayerCalloutDialog/ArrayDump.kt
@@ -1,11 +1,12 @@
 package com.bgvofir.grappygis.LayerCalloutDialog
 
 object ArrayDump{
-    fun getItem(): ArrayList<String> {
-        val items = arrayListOf<String>("קו מים",
-                "פלוצלציה",
-                "מגופים"
-        )
-        return items
+    fun getItem(): MutableMap<String, String> {
+        val myMap = mutableMapOf<String, String>()
+        myMap.put("דור","גלי")
+        myMap.put("רן", "טל")
+        myMap.put("אופיר", "?")
+        myMap.put("אורן", "יד ימין")
+        return myMap
     }
 }
