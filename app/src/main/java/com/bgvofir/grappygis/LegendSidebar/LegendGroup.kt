a255880e87b11dca9dbee8e4d8c6f9c22696707a
Ran Loock <ran.trisk@gmail.com>
2019-12-27 17:13:06 +0200

    updates for ecw

--- app/src/main/java/com/bgvofir/grappygis/LegendSidebar/LegendGroup.kt
+++ app/src/main/java/com/bgvofir/grappygis/LegendSidebar/LegendGroup.kt
@@ -1,5 +1,5 @@
 package com.bgvofir.grappygis.LegendSidebar
 
 class LegendGroup(){
-
+    
 }
--- app/src/main/java/com/bgvofir/grappygis/LegendSidebar/LegendLayerDisplayController.kt
+++ app/src/main/java/com/bgvofir/grappygis/LegendSidebar/LegendLayerDisplayController.kt
@@ -1,5 +1,13 @@
 package com.bgvofir.grappygis.LegendSidebar
 
-class LegendLayerDisplayController (){
+import com.google.firebase.storage.FirebaseStorage
 
+object LegendLayerDisplayController{
+    val storage = FirebaseStorage.getInstance()
+
+    fun fetchMMap(projectID: String){
+        val storageReference = storage.reference
+        val storageRef = storageReference.child("settlements/$projectID/layers/points.json")
+
+    }
 }
